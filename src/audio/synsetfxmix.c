d8f54756e19f7373f1c379c2fdc5e387db00e374
Chris Lewis <me@chrislewis.au>
2024-11-29 21:21:00 -0500

    Force src/audio/synsetfxmix.c to version I

--- src/audio/synsetfxmix.c
+++ src/audio/synsetfxmix.c
@@ -39,18 +39,13 @@ void alSynSetFXMix(ALSynth *synth, ALVoice *v, u8 fxmix)
          */
         update->delta  = synth->paramSamples + v->pvoice->offset;
         update->type   = AL_FILTER_SET_FXAMT;
-#if BUILD_VERSION >= VERSION_J
-        if (fxmix > 127) {
-            fxmix = 127;
-        }
-	    update->data.i = fxmix;
-#else
+
         if (fxmix < 0) { // Not possible
             update->data.i = -fxmix;
         } else {
             update->data.i = fxmix;
         }
-#endif
+
         update->next   = 0;
 
         f = v->pvoice->channelKnob;
