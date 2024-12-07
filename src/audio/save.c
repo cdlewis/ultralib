112c2d1ae22b1cb0b90cf9f7f6d6fb900ee9ebc3
Chris Lewis <me@chrislewis.au>
2024-12-07 17:35:04 -0500

    force debug asserts and tweak save line numbers

--- src/audio/save.c
+++ src/audio/save.c
@@ -29,10 +29,9 @@ Acmd *alSavePull(void *filter, s16 *outp, s32 outCount, s32 sampleOffset,
     Acmd        *ptr = p;
     ALSave *f = (ALSave *)filter;
     ALFilter    *source = f->filter.source;
-#if BUILD_VERSION < VERSION_J
-#line 33
-#endif
-    assert(f->filter.source);
+
+
+    force_assert(f->filter.source);
 
     
     ptr = (*source->handler)(source, outp, outCount, sampleOffset, ptr);
