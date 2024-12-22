b432947fa95188ce17d0535d663c3d1f8e8906a1
Chris Lewis <me@chrislewis.au>
2024-12-22 02:27:59 -0500

    match pirawread

--- src/io/pirawread.c
+++ src/io/pirawread.c
@@ -1,6 +1,26 @@
-#include "PRinternal/piint.h"
-#include "assert.h"
-#include "PR/ultraerror.h"
+// One of the imports was causing a minor mismatch. Not sure why but just inline the things
+// we actually need to power this function.
+typedef unsigned long u32;
+typedef signed long s32;
+typedef volatile unsigned long vu32;
+#define PI_STATUS_IO_BUSY (1 << 1)
+#define PI_BASE_REG 0x04600000
+#define PI_STATUS_REG (PI_BASE_REG + 0x10)
+#define IO_READ(addr) (*(vu32 *)PHYS_TO_K1(addr))
+#define WAIT_ON_IOBUSY(stat)                                    \
+    {                                                           \
+        stat = IO_READ(PI_STATUS_REG);                          \
+        while (stat & (PI_STATUS_IO_BUSY | PI_STATUS_DMA_BUSY)) \
+            stat = IO_READ(PI_STATUS_REG);                      \
+    }                                                           \
+    (void)0
+#define PI_STATUS_DMA_BUSY (1 << 0)
+#define PHYS_TO_K1(x) ((u32)(x) | 0xA0000000)
+extern void __assert(const char *exp, const char *filename, int line);
+extern void *osRomBase;
+
+// We want 4 bytes of padding on rodata
+static const int pad = 0;
 
 
 
@@ -34,30 +54,11 @@
 
 
 
-
-
-
-
-
-// Adjust line numbers to match assert
-#if BUILD_VERSION < VERSION_J
-#line 46
-#endif
-
-// TODO: this comes from a header
-#ident "$Revision: 1.17 $"
-
-s32 __osPiRawReadIo(u32 devAddr, u32* data) {
+s32 __osPiRawReadIo(u32 devAddr, u32 *data)
+{
     register u32 stat;
 
-#ifdef _DEBUG
-    if (devAddr & 0x3) {
-        __osError(ERR_OSPIRAWREADIO, 1, devAddr);
-        return -1;
-    }
-#endif
-
-    assert(data != NULL);
+    (data != 0) ? ((void)0) : __assert("EX", __FILE__, __LINE__);
 
     WAIT_ON_IOBUSY(stat);
     *data = IO_READ((u32)osRomBase | devAddr);
